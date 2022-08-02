6bc85152e630a7f37e0642c67263019c191b80d6
xiemeiping <1035791926@qq.com>
2022-08-02 11:17:26 +0800

    change the navigation bar

--- docs/conf.py
+++ docs/conf.py
@@ -52,16 +52,16 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # SunFounder logo
 
 #html_js_files = [
-#    'https://ezblock.cc/readDocFile/topHead.js',
+#    'https://ezblock.cc/readDocFile/custom.js',
 #]
 #html_css_files = [
-#    'https://ezblock.cc/readDocFile/topHead.css',
+#    'https://ezblock.cc/readDocFile/custom.css',
 #]
 
 #### RTD+
 
 html_js_files = [
-    'https://ezblock.cc/readDocFile/topHead.js',
+    'https://ezblock.cc/readDocFile/custom.js',
     'https://ezblock.cc/readDocFile/readTheDoc/src/js/ace.js',
     'https://ezblock.cc/readDocFile/readTheDoc/src/js/ext-language_tools.js',
     'https://ezblock.cc/readDocFile/readTheDoc/src/js/theme-chrome.js',
@@ -74,7 +74,7 @@ html_js_files = [
 
 ]
 html_css_files = [
-    'https://ezblock.cc/readDocFile/topHead.css',
+    'https://ezblock.cc/readDocFile/custom.css',
     'https://ezblock.cc/readDocFile/readTheDoc/src/css/index.css',
     'https://ezblock.cc/readDocFile/readTheDoc/src/css/xterm.css',
 ]
